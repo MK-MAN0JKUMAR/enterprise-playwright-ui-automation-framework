3aca810d9695cfbe1560419333d6fbdfc2174ab5
Manoj Kumar <manojkr5812@gmail.com>
2026-04-17 18:55:49 +0530

    Updated leave page and now user can apply for leave

--- src/applications/orangehrm/pages/LeavePage.ts
+++ src/applications/orangehrm/pages/LeavePage.ts
@@ -71,6 +71,8 @@ export class LeavePage extends BasePage {
 
     await this.init();
 
+    await this.openApplyLeave();
+
     // select leave type
     await this.leaveTypeDropdown.open();
 
@@ -98,47 +100,3 @@ export class LeavePage extends BasePage {
   // */
 
 }
-
-
-/*
--click on leave list from dashboard 
-  await page.getByRole('link', { name: 'Leave' }).click();
-
--Verify the leave page is loaded by checking the presence of the heading
-  await page.getByRole('heading', { name: 'Leave', exact: true }).click();
-
--click on apply button
-  await page.getByRole('link', { name: 'Apply' }).click();
-
--click on the leave type dropdown 
-  await page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow').click();
-
-  -apply for CAN - FMLA leave type
-  await page.getByRole('option', { name: 'CAN - FMLA' }).click();
-
--verify leave availability message is displayed
-  await page.getByText('Day(s)').click();
-
- - select from date 
-  await page.locator('.oxd-icon.bi-calendar').first().click();
-  
-  - select 24th from the calendar
-  await page.getByText('24').click();
-  
-  - select input box for comments and enter "Urgent work"
-  await page.locator('textarea').click();
-  await page.locator('textarea').fill('Urgent work');
-  
-  - click on apply button to submit the leave request
-  await page.getByRole('button', { name: 'Apply' }).click();
-  
-- verify the success message is displayed
-  await page.getByText('Successfully Saved').click();
-  
--click on My Leave for check on navbar
-  await page.getByRole('listitem').filter({ hasText: 'My Leave' }).click();
-  
-- verify the applied leave request is displayed in the list any thing but more then 1 record should not be there as we have applied only one leave request
-  await page.getByText('(1) Records Found').click();
-
-*/
